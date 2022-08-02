db8011012932e0051ebe3646adb5973e6e64dcdc
Dhiraj <Dhiraj@192.168.0.102>
2022-08-02 16:20:25 +0530

    Forth Commit

---
+++ Day-2 Java_Intro/src/com/capgemini/github/IntegerCompileInput.java
@@ -0,0 +1,15 @@
+package com.capgemini.github;
+//program on Input Integer
+
+import java.util.Scanner;
+public class IntegerCompileInput {
+
+	public static void main(String[] args) {
+		//int X;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the Value of X");
+		int X= sc.nextInt();
+		System.out.println(X);
+	}
+
+}
---
+++ Day-2 Java_Intro/src/com/capgemini/github/IntegerCompiler.java
@@ -0,0 +1,11 @@
+package com.capgemini.github;
+//program on Integer data-type
+public class IntegerCompiler {
+
+	public static void main(String[] args) {
+		int a=145;
+		System.out.println(a);
+;
+	}
+
+}

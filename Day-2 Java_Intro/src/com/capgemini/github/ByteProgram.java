6241366ff2e6086c3ebdc1b05d647686f9d2727f
Dhiraj <Dhiraj@192.168.0.102>
2022-08-02 15:44:39 +0530

    Third Commit

---
+++ Day-2 Java_Intro/src/com/capgemini/github/ByteProgram.java
@@ -0,0 +1,13 @@
+package com.capgemini.github;
+// profram for byte data-type
+public class ByteProgram {
+
+	public static void main(String[] args) {
+		byte a=127;
+		// byte a=129
+		// it will give you an error because of byte range
+		System.out.println(a);
+
+	}
+
+}

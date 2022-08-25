0dc2c4f9bed17f93345bb122459c430a7549dfc8
Dhiraj <Dhiraj@DESKTOP-51FJO41>
2022-08-25 15:21:26 +0530

    Third Commit

--- Day-16 Generics/src/com/cg/generics/Executor.java
+++ Day-16 Generics/src/com/cg/generics/Executor.java
@@ -1,10 +1,27 @@
 package com.cg.generics;
-
 public class Executor {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Sender<String> obj=new Sender<>();
+		obj.setMessege("Generic class demo");
+		obj.sendmessege();
+		System.out.println();
+		
+		// create an email object to be send using sender 
+		Email em=new Email();
+		em.setFrom("sdesale50@gmail.com");
+		em.setTo("sumitdesale9@gmail.com");
+		em.setSubject("full stack");
+	   em.setBody("About training process");
+	   System.out.println(em.getBody());
+	   
+	   //create sender to send email 
+	   Sender<Email>obj1=new Sender<Email>();
+	  obj1.setMessege(em);
+	
+	  obj1.sendmessege();
+	 
+	 
 	}
 
 }
+
